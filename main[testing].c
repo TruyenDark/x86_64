05b840babe1fbd84f8f46e13cd2b47f5885679a9
TruyenDark <poison999147@gmail.com>
2026-04-04 18:08:16 +0700

    Add files via upload

---
+++ main[testing].c
@@ -0,0 +1,15 @@
+#ifdef _WIN64
+	#include <stdio.h>
+	extern void printString(const char* p);
+	extern void printHexNum(long long num);
+	extern void printUnsignDecNum(long long num);
+	extern void printSignDecNum(long long num);
+	int main(void) {
+		printString("Hello World!\n");
+		printHexNum(0xffffffffffffffff);
+		printUnsignDecNum(0xffffffffffffffff);
+		printSignDecNum(0xffffffffffffffff);
+		return 0;
+	}
+#endif
+/*This is a testing program, I created it for you so you just need to run it and check for bugs*/
