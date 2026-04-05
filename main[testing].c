9a2811ac15f91041c6a017e6dc2161f76f66c9fa
TruyenDark <poison999147@gmail.com>
2026-04-05 14:32:22 +0700

    Add files via upload

--- main[testing].c
+++ main[testing].c
@@ -1,4 +1,4 @@
-#ifdef _WIN64
+#if defined(_WIN64) && defined(_M_X64) || defined(_M_ARM64)
 	#include <stdio.h>
 	#include <windows.h>
 	extern void printString(const char* p);
@@ -12,5 +12,9 @@
 		printSignDecNum(0xffffffffffffffff);
 		return 0;
 	}
+#elif defined(__ANDROID__) && defined(__aarch64__)
+	# error "Actually, I already have a draft for the AArch64 architecture. But because I'm lazy, I haven't uploaded it yet, so I'm using some professional compiler to translate it into ARM64 Assembly for now."
+#elif defined(__linux__) && defined(__x86_64__) || defined(__aarch64__)
+	# error "Use the MSVC (Microsoft Visual C++) compiler from Windows 64-bit to build various Linux distributions."
 #endif
 /*This is a testing program, I created it for you so you just need to run it and check for bugs*/
